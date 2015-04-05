8b922d44978d7513b446cda11a59724db58669a1
Jake Lundberg <jlundberg@adconion.com>
2015-04-05 02:01:32 -0700

    master: updating for new filenames

--- modules/configs/manifests/init.pp
+++ modules/configs/manifests/init.pp
@@ -2,23 +2,24 @@
 
 class config {
   
-  file {"${env_home}/.vimrc" :
-    ensure  => present,
-    source  => "puppet:///modules/config/vimrc_${osfamily}",
-    owner   => "${env_user}",
+  file {"${env_home}/.vim" :
+    ensure  => directory,
+    recurse => true,
     mode    => 0644,
+    source  => "puppet:///modules/config/vim/",
+    owner   => "${env_user}",
   }
 
   file {"${env_home}/.bash_profile" :
     ensure  => present,
-    source  => "puppet:///modules/config/bash_profile_${osfamily}",
+    source  => "puppet:///modules/config/bash_profile",
     owner   => "${env_user}",
     mode    => 0644,
   }
 
   file {"${env_home}/.bashrc" :
     ensure  => present,
-    source  => "puppet:///modules/config/bashrc_${osfamily}",
+    source  => "puppet:///modules/config/bashrc",
     owner   => "${env_user}",
     mode    => 0644,
   }
@@ -29,9 +30,5 @@ class config {
     owner   => "${env_user}",
     mode    => 0644,
   }
-
-
-
-
 
 }
