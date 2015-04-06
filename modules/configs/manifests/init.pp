937dc682db91aed4301acddfd83af3dc3281d966
Jake Lundberg <jlundberg@adconion.com>
2015-04-06 11:55:20 -0700

    master: making local configs dynamic

--- modules/configs/manifests/init.pp
+++ modules/configs/manifests/init.pp
@@ -1,13 +1,36 @@
 # This class installs personal configuration files 
 
-class config {
-  
-  file {"${env_home}/.vim" :
+class config ( $local_repo = undef ) {
+
+  file { "${env_home}/.vimrc" :
+    ensure  => present,
+    recurse => true,
+    mode    => 0644,
+    source  => "puppet:///modules/config/vimrc/",
+    owner   => "${env_user}",
+  }
+
+ 
+  # Entire directory structure to use VIM plugin style 
+  # Settings
+  file { "${env_home}/.vim" :
     ensure  => directory,
     recurse => true,
     mode    => 0644,
     source  => "puppet:///modules/config/vim/",
     owner   => "${env_user}",
+  }
+
+  # Extra environment variables, functions, aliases, etc
+  # that are location specific.  Probably differs by company
+  if $local_repo {
+    vcsrepo { "${env_home}/local_config" :
+      ensure   => latest,
+      provider => git,
+      owner    => "${env_user}",
+      user     => "${env_user}",
+      source   => "${local_repo}",
+    }
   }
 
   file {"${env_home}/.bash_profile" :
